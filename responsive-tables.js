cfe0ab897ac61a107baa51d2bcb0333f4bf6c0c8
NicholasRBowers <nicholas.ryan.bowers@gmail.com>
2015-04-14 21:23:06 -0400

    Incorporated changes from this pull request:  https://github.com/zurb/responsive-tables/pull/21.
    Fixes issues:
    - https://github.com/zurb/responsive-tables/issues/15
    - https://github.com/zurb/responsive-tables/issues/18
    - https://github.com/zurb/responsive-tables/pull/21
    
    NOTE:  I could not pull the PR manually, as the the changes were from an "unknown repository". Credit for this commit should go to https://github.com/seriouslysean.

--- responsive-tables.js
+++ responsive-tables.js
@@ -23,47 +23,44 @@
     $(window).on("resize", updateTables);
 
 
-  	function splitTable(original)
-  	{
-  		original.wrap("<div class='table-wrapper' />");
+    function splitTable(original)
+    {
+      original.wrap("<div class='table-wrapper' />");
 
-  		var copy = original.clone();
-  		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-  		copy.removeClass("responsive");
+      var copy = original.clone();
+      copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
+      copy.removeClass("responsive");
 
-  		original.closest(".table-wrapper").append(copy);
-  		copy.wrap("<div class='pinned' />");
-  		original.wrap("<div class='scrollable' />");
+      original.closest(".table-wrapper").append(copy);
+      copy.wrap("<div class='pinned' />");
+      original.wrap("<div class='scrollable' />");
 
       setCellHeights(original, copy);
-  	}
+      setTableHeights(original, copy);
+    }
 
-  	function unsplitTable(original) {
+    function unsplitTable(original) {
       original.closest(".table-wrapper").find(".pinned").remove();
       original.unwrap();
       original.unwrap();
-  	}
+    }
 
     function setCellHeights(original, copy) {
-      var tr = original.find('tr'),
-          tr_copy = copy.find('tr'),
-          heights = [];
-
-      tr.each(function (index) {
-        var self = $(this),
-            tx = self.find('th, td');
-
-        tx.each(function () {
-          var height = $(this).outerHeight(true);
-          heights[index] = heights[index] || 0;
-          if (height > heights[index]) heights[index] = height;
-        });
-
+      $('tr', original).each(function(index){
+        oheight = $(this).height();
+        cheight = $('tr:eq('+index+')', copy).height();
+        height = (oheight > cheight ? oheight : cheight);
+        $('th,td', original).css('height', height+'px');
+        $('th,td', copy).css('height', height+'px');
       });
+    }
 
-      tr_copy.each(function (index) {
-        $(this).height(heights[index]);
-      });
+    function setTableHeights(original, copy) {
+        oheight = original.outerHeight();
+        cheight = copy.outerHeight();
+        height = (oheight > cheight ? oheight : cheight);
+        original.css('min-height', height+'px');
+        copy.css('min-height', height+'px');
     }
 
   });
